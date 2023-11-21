4c4856aad06dd5ef0964e6078bd5f4346a41eb04
callmeclover <cats11134@gmail.com>
2023-11-21 19:21:14 +0000

    woohoo progress

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*let test;
+let gui = createGui('Settings');
+gui.addGlobals(test)*/
+
 function contains(arr, value) {
   var i = arr.length;
   while (i--) {
@@ -23,7 +27,7 @@ function setup() {
   sprite.rotationLock = true;
   sprite.bounciness = 0.15;
   sprite.friction = 1.25;
-  sprite.color = 'rgb(178, 23, 103)'
+  sprite.color = "rgb(178, 23, 103)";
 
   textFont("Orbit");
 
@@ -42,22 +46,6 @@ function draw() {
   text(`fps: ${int(getFrameRate())}`, 15, 25);
   text(`xvel: ${Math.round(float(sprite.vel.x) * 10) / 10}`, 15, 41);
   text(`yvel: ${Math.round(float(sprite.vel.y) * 10) / 10}`, 15, 56);
-  text(`jumps: ${jumps}`, 15, 77);
-
-  camera.on();
-
-  textAlign(CENTER)
-  fill(255)
-  textSize(24)
-  text("Welcome to Insurrection!", 0, 200)
-  textSize(16)
-  text("Use ⮜ and ⮞ to move.", 0, 232)
-  textSize(16)
-  text("Use ⮝ to jump.", 550, 402)
-  textSize(12)
-  text("(you can double jump, too.)", 550, 418)
-
-  camera.off();
 
   if (sprite.collide(window.floors)) {
     jumps = maxJumps;
@@ -65,7 +53,7 @@ function draw() {
 
   if (theKey == "z" && sprite.collide(window.walls)) {
     jumps = maxJumps;
-    theKey = ''
+    theKey = "";
   }
 
   if (
@@ -92,6 +80,50 @@ function draw() {
 
   camera.x = sprite.x;
   camera.y = sprite.y;
+
+  if (sprite.y > 1500) {
+    sprite = new Sprite(0, 250, 30, 30);
+    sprite.rotationLock = true;
+    sprite.bounciness = 0.15;
+    sprite.friction = 1.25;
+    sprite.color = "rgb(178, 23, 103)";
+  }
+
+  camera.on();
+
+  textAlign(CENTER);
+  fill(255);
+  textSize(24);
+  text("Welcome to Insurrection!", 0, 200);
+  textSize(16);
+  text("Use ⮜ and ⮞ to move.", 0, 232);
+
+  textSize(16);
+  text("Use ⮝ to jump.", 550, 402);
+  textSize(12);
+  text("(you can double jump, too.)", 550, 418);
+
+  textSize(16);
+  text("Some platforms are bouncy.", 900, 500);
+  text("But we need more height!", 1100, 0);
+  textSize(14);
+  text("Press ⮟ to ground-pound.", 1100, 20);
+
+  textSize(16);
+  text("Nice job!", 1500, 0);
+  textSize(14);
+  text("Just a few last things...", 1500, 20);
+
+  textSize(16);
+  text("Don't worry!", 1700, 700);
+  textSize(12);
+  text("(this is scripted, after all.)", 1700, 720);
+  textSize(16);
+  text("You'll see an enemy soon!", 2100, 700);
+  text("It can't do anything now, though.", 2100, 720);
+  text("Ground-pound him!", 2100, 740);
+
+  camera.off();
 }
 
 function keyPressed() {
--- level.js
+++ level.js
@@ -5,25 +5,61 @@ function createLevel() {
   window.floors = new Group();
   window.walls = new Group();
   window.ceils = new Group();
+  window.bfloors = new floors.Group();
+  window.sfloors = new floors.Group();
 
   // Create floors
   new window.floors.Sprite(100, 500, 700, 20, "s");
   new window.floors.Sprite(640, 450, 400, 20, "s");
+  new window.floors.Sprite(1190, 100, 395, 20, "s");
+
+  new window.floors.Sprite(1580, 100, 395, 20, "n").stroke = 0; // Fake-out floor to teach enemies
+
+  new window.floors.Sprite(1780, 800, 790, 20, "s");
 
   // Create ceilings
   new window.ceils.Sprite(0, 0, 500, 20, "s");
+  new window.ceils.Sprite(540, 200, 560, 20, "s");
+  new window.ceils.Sprite(1309, -135, 955, 20, "s");
 
   // Create walls
   new window.walls.Sprite(-250, 250, 20, 520, "s");
   new window.walls.Sprite(250, 195, 20, 410, "s");
 
   new window.walls.Sprite(450, 475, 20, 70, "s");
+  new window.walls.Sprite(825, 110, 20, 510, "s");
+  new window.walls.Sprite(985, 275, 20, 370, "s");
+  new window.walls.Sprite(1785, -17, 20, 255, "s");
 
   // Create special objects (and set their properties)
-  // new window.floors.Sprite(width * 0.25, height * 0.75, 100, 10, "s").bounciness = 1;
+  new window.bfloors.Sprite(910, 460, 150, 20, "s").bounciness = 1;
   // new window.floors.Sprite(width * 0.75, height * 0.75, 100, 10, "s").friction = 0;
 
-  walls.color = `rgb(${chance.integer({min: 50, max: 180})}, ${chance.integer({min: 54, max: 64})}, ${chance.integer({min: 150, max: 160})})`
-  ceils.color = `rgb(${chance.integer({min: 50, max: 180})}, ${chance.integer({min: 54, max: 64})}, ${chance.integer({min: 150, max: 160})})`
-  floors.color = `rgb(${chance.integer({min: 50, max: 180})}, ${chance.integer({min: 54, max: 64})}, ${chance.integer({min: 150, max: 160})})`
+  walls.color = `rgb(${chance.integer({ min: 50, max: 180 })}, ${chance.integer(
+    { min: 54, max: 64 }
+  )}, ${chance.integer({ min: 150, max: 160 })})`;
+  ceils.color = `rgb(${chance.integer({ min: 50, max: 180 })}, ${chance.integer(
+    { min: 54, max: 64 }
+  )}, ${chance.integer({ min: 150, max: 160 })})`;
+  floors.color = `rgb(${chance.integer({
+    min: 50,
+    max: 180,
+  })}, ${chance.integer({ min: 54, max: 64 })}, ${chance.integer({
+    min: 150,
+    max: 160,
+  })})`;
+  bfloors.color = `rgb(${chance.integer({
+    min: 50,
+    max: 180,
+  })}, ${chance.integer({ min: 54, max: 94 })}, ${chance.integer({
+    min: 90,
+    max: 160,
+  })})`;
+  sfloors.color = `rgb(${chance.integer({
+    min: 50,
+    max: 180,
+  })}, ${chance.integer({ min: 54, max: 94 })}, ${chance.integer({
+    min: 90,
+    max: 160,
+  })})`;
 }
